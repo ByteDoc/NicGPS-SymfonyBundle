fdf7a634c7a492d25b36c58e37c1ef3ca09953aa
Max Schaufler <bytedoc@bytedoc.net>
2013-12-30 22:05:32 +0100

    Database attached
    Read Controller now fetches data from the database, and returns json
    data
    First Entity “Webressource” created

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -3,11 +3,23 @@
 namespace Bytedoc\GpsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ByteDoc\GpsBundle\Entity\Webressource;
 
 class MainController extends Controller
 {
     public function indexAction($name)
     {
+		$webressource = new Webressource();
+		$webressource->setGuid("zyxwvu");
+		$webressource->setCategory("speziell");
+		$webressource->setTitle("zweiter Eintrag");
+		$webressource->setHref("http://blog.bytedoc.net");
+		$webressource->setRating(3);
+		
+		$em = $this->getDoctrine()->getManager();
+		//$em->persist($webressource);
+		$em->flush();
+		
         return $this->render('BytedocGpsBundle:Main:' . $name . '.html.twig');
     }
 }
--- Controller/ReadController.php
+++ Controller/ReadController.php
@@ -6,60 +6,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
+
 class ReadController extends Controller
 {
-    public function indexAction($dataset)
+    public function readAction($entity)
     {
-
-		//header('Content-Type: application/json');
-
-		// read the requested dataset
-		//$dataset = $_REQUEST['dataset'];
-
-		// check for allowed characters only
-		if(preg_match('/^[a-z0-9\-\_]+$/i', $dataset)) {
-			// try to read existing json file for requested dataset
-			$filename = "data/" . $dataset . ".json";
-
-			// Data in File stored as JSON Format
-			if(file_exists($filename)) {
-				$jsonString = file_get_contents($filename);
-			} else {
-				$jsonString = "[]";
-			}
-		} else {
-			$jsonString = "[]";
-		}
-		$jsonString = stripslashes($jsonString);
-		//echo $jsonString; exit();
+		$repository = $this->getDoctrine()->getRepository("BytedocGpsBundle:".$entity);
+		$objects = $repository->findAll();
 		
-
-		//$jsonString = getcwd();
-
-        //return $this->render('ByteDocHelloBundle:Read:json.html.twig', array('json_string' => $jsonString));
+		$jsonString = $this->serializeToJson($objects);
 		
-		// alternative rendering by creating a reponse object
-		$content = $this->renderView('ByteDocHelloBundle:Read:json.html.twig', array('json_string'  => $jsonString));
+		return $this->getJsonResponse($jsonString);
+    }
+	
+	private function serializeToJson($object)
+	{
+		$encoders = array(new XmlEncoder(), new JsonEncoder());
+		$normalizers = array(new GetSetMethodNormalizer());
+
+		$serializer = new Serializer($normalizers, $encoders);
+		
+		return $serializer->serialize($object, 'json');
+	}
+	
+	private function getJsonResponse($jsonString)
+	{
+		// Create a response object with JSON content header
+		$content = $this->renderView('BytedocGpsBundle:Read:json.html.twig', array('json_string'  => $jsonString));
 		$statusCode = 200;
 		$responseHeaders = array('content-type' => 'application/json');
 		return new Response($content, $statusCode, $responseHeaders);
 		
-		
 		// creating a JSON Reponse
 		// wird noch als Unicode ausgegeben, das muss ich mir dann noch anschauen...
-		// ... wenn ich die Daten nicht aus einer Datei sondern aus der Datenbank hole ...
-		// ... und demnach als Array bzw. als Objekt verarbeiten kann
-		$response = new JsonResponse();
-		//$response->setData($jsonString);
-		$data = array( 'modelData' => array(
-			//array(lastName => 'Dente', name => 'Max', 'checked' => true, 'linkText' => 'www.sap.com', 'href' => 'http://www.sap.com/', 'rating' => 4),
-			array('lastName' => 'Friese', 'name' => 'Andy', 'checked' => true, 'linkText' => 'https://experience.sap.com/fiori', 'href' => 'https://experience.sap.com/fiori', 'rating' => 2),
-			array('lastName' => 'Mann', 'name' => 'Anita', 'checked' => false, 'linkText' => 'www.sap.com', 'href' => 'http://www.sap.com/', 'rating' => 3)
-			)
-		);
-		$response->setData($data);
-		
+		//$response = new JsonResponse();
+		//$response->setData($jsonContent);
 		//return $response;
-		
-    }
+	}
 }
